3c36fc35145921252379b377a20e8999a447744d
otaviolaskosky7 <laskotavio@gmail.com>
2024-07-04 09:28:35 -0300

    Class 13

--- src/PortfolioContainer/Home/Profile.js
+++ src/PortfolioContainer/Home/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Typical from 'react-typical'
+import "./Profile.css";
 
 export default function Profile(){
     return (
@@ -7,21 +8,23 @@ export default function Profile(){
             <div className='profile-parent'>
                 <div className='profile-details'>
                     <div className='colz'>
-                        {/* <a href='#'>
-                            <i className='fa fa-facebook-square'></i>
-                        </a>
-                        <a href='#'>
-                            <i className='fa fa-google-plus-square'></i>
-                        </a>
-                        <a href='#'>
-                            <i className='fa fa-instagram'></i>
-                        </a>
-                        <a href='#'>
-                            <i className='fa fa-youtube-square'></i>
-                        </a> */}
-                        <a href='https://x.com/OLaskosky7'>
-                            <i className='fa fa-twitter'></i>
-                        </a>
+                        <div className='colz-icon'>
+                            {/* <a href='#'>
+                                <i className='fa fa-facebook-square'></i>
+                            </a>
+                            <a href='#'>
+                                <i className='fa fa-google-plus-square'></i>
+                            </a>
+                            <a href='#'>
+                                <i className='fa fa-instagram'></i>
+                            </a>
+                            <a href='#'>
+                                <i className='fa fa-youtube-square'></i>
+                            </a> */}
+                            <a href='https://x.com/OLaskosky7'>
+                                <i className='fa fa-twitter'></i>
+                            </a>
+                        </div>
                     </div>
                     <div className='profile-details-name'>
                         <span className='primary-text'>
@@ -33,7 +36,6 @@ export default function Profile(){
                         <span className='primary-text'>
                             {" "}
                             <h1>
-                                {" "}
                                 <Typical 
                                     loop={Infinity}
                                     steps={[
@@ -50,20 +52,23 @@ export default function Profile(){
                                     ]}
                                 />
                             </h1>
-                            <span className='profile-role-tagline'>
-                                Knack of building applications with front and back and operations.
-                            </span>
+                        </span>
+                        <span className='profile-role-tagline'>
+                            Knack of building applications with front and back and operations.
                         </span>
                     </div>
-                    <div className='profile-optins'>
+
+                    <div className='profile-options'>
                         <button className='btn primary-btn'>
-                            {""}
-                            Hire Me{" "}
+                            Hire Me
                         </button>
                         <a href='laskoskycv.pdf' download="Laskosky laskosky.pdf">
                             <button className='btn highlighted-btn'>Get Resume</button>
                         </a>
                     </div>
+                </div>
+                <div className='profile-picture'>
+                    <div className='profile-picture-background'></div>
                 </div>
             </div>
         </div>
